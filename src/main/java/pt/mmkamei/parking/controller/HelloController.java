a2ac3765b707b0240dcbe2f5898cf177b5801805
marcelokamei <marcelokamei@hotmail.com>
2022-08-18 20:24:07 +0100

    Estrutura inicial

---
+++ src/main/java/pt/mmkamei/parking/controller/HelloController.java
@@ -0,0 +1,14 @@
+package pt.mmkamei.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+    @GetMapping
+    public String Hello(){
+        return "<h1>Hello Cloud Parking... Devs</h1>";
+    }
+}
