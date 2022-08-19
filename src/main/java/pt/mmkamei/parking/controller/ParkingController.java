b5f49aed6398d4236873fd60f2a4b5f6525a7b8c
marcelokamei <marcelokamei@hotmail.com>
2022-08-19 10:50:51 +0100

    Adicionando Swagger 2.9.2

--- src/main/java/pt/mmkamei/parking/controller/ParkingController.java
+++ src/main/java/pt/mmkamei/parking/controller/ParkingController.java
@@ -1,10 +1,11 @@
 package pt.mmkamei.parking.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pt.mmkamei.parking.controller.mapper.ParkingMapper;
-import pt.mmkamei.parking.dto.ParkingDTO;
+import pt.mmkamei.parking.controller.dto.ParkingCreateDTO;
+import pt.mmkamei.parking.controller.dto.ParkingDTO;
 import pt.mmkamei.parking.model.Parking;
 import pt.mmkamei.parking.service.ParkingService;
 
@@ -23,9 +24,26 @@ public class ParkingController {
     }
 
     @GetMapping
-    public List<ParkingDTO> findAll(){
+    public ResponseEntity<List<ParkingDTO>> findAll() {
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-        return result;
+        return ResponseEntity.ok(result);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable String id) {
+        Parking parking = parkingService.findById(id);
+        ParkingDTO result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto) {
+        var parkingCreate = parkingMapper.toParkingCreate(dto);
+        var parking = parkingService.create(parkingCreate);
+        var result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+
 }
--- src/main/java/pt/mmkamei/parking/service/ParkingService.java
+++ src/main/java/pt/mmkamei/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package pt.mmkamei.parking.service;
 import org.springframework.stereotype.Service;
 import pt.mmkamei.parking.model.Parking;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,9 +16,15 @@ public class ParkingService {
     private static Map<String, Parking> parkingMap = new HashMap<>();
 
     static {
-        var id = getUUID();
-        Parking parking = new Parking(id, "KMK-4545", "RO", "Polo", "Amarelo");
-        parkingMap.put(id, parking);
+        var id1 = getUUID();
+        var id2 = getUUID();
+        var id3 = getUUID();
+        Parking parking = new Parking(id1, "KMK-4545", "RO", "Polo", "Amarelo");
+        Parking parking1 = new Parking(id2, "KKK-1234", "MS", "Gol", "Preto");
+        Parking parking2 = new Parking(id3, "aaa-4567", "PR", "Vectra", "Branco");
+        parkingMap.put(id1, parking);
+        parkingMap.put(id2, parking1);
+        parkingMap.put(id3, parking2);
     }
 
     public List<Parking> findAll(){
@@ -26,5 +33,17 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
